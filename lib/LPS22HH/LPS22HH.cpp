7baf4df8ebee156b24a37a6782846854ad753bbc
ardatoker <67735114+venomni@users.noreply.github.com>
2025-05-04 23:09:27 +0300

    Update LPS22HH.cpp

--- lib/LPS22HH/LPS22HH.cpp
+++ lib/LPS22HH/LPS22HH.cpp
@@ -7,6 +7,9 @@ LPS22HH::LPS22HH(I2Class* _i2c) {
 uint8_t LPS22HH::lpsReadBytes(uint8_t regadr, uint8_t* temp, uint8_t length, uint16_t timeout){
     return this->i2c->I2CReadBytes(CHIP_ADDRESS, regadr, temp, length, timeout);
 }
+bool LPS22HH::lpsWriteByte(uint8_t regadr, uint8_t data){
+    return this->i2c->I2CWriteByte(CHIP_ADDRESS, regadr, data);
+}
 
 float LPS22HH::getPressure() {
     uint8_t buffer[3] = {0};
@@ -31,4 +34,24 @@ bool LPS22HH::readWhoAmI() {
     uint8_t buffer = 0;
     this->lpsReadBytes(WHO_AM_I, &buffer, 1);
     return buffer == 0b10110011;
+}
+
+void LPS22HH::setCTRL_REG1(LPS_OUTPUT_DATA_RATE odrRate, LPS_EN_LPFP lpfp, LPS_BDU bdu) {
+    uint8_t temp = 0;
+
+    temp |= ((uint8_t)lpfp << 3) | (uint8_t)odrRate << 4 | (uint8_t)bdu << 1;
+    this->lpsWriteByte(CTRL_REG1, temp);
+
+}
+void LPS22HH::setCTRL_REG2(LPS_LOW_NOISE lpsLowNoise) {
+    uint8_t temp = 0;
+    temp |= ((uint8_t)lpsLowNoise << 1);
+    this->lpsWriteByte(CTRL_REG2, temp);
+}
+void LPS22HH::resetLPS() {
+    uint8_t temp = 0;
+
+    temp |= (1 << 2);
+    this->lpsWriteByte(CTRL_REG2, temp);
+    delay(100);
 }
