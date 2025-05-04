3a7ed8fc8fb2b268985a233820a7264c2ef0ef78
ardatoker <67735114+venomni@users.noreply.github.com>
2025-05-04 22:01:05 +0300

    Update LPS22HH.cpp

--- lib/LPS22HH/LPS22HH.cpp
+++ lib/LPS22HH/LPS22HH.cpp
@@ -1,23 +1,23 @@
 #include "LPS22HH.h"
 uint8_t LPS22HH::lpsReadBytes(uint8_t regadr, uint8_t* temp, uint8_t length, uint16_t timeout){
-    return this->_i2c->I2CReadBytes(BMP_CHIPADR, regadr, temp, length, timeout);
+    return this->i2c->I2CReadBytes(CHIP_ADDRESS, regadr, temp, length, timeout);
 }
-float getPressure() {
+float LPS22HH::getPressure() {
     uint8_t buffer[3];
-    readBytes(CHIP_ADDRESS, PRESSURE_OUT_XL, buffer, 3);
+    this->lpsReadBytes(PRESSURE_OUT_XL, buffer, 3, TIMEOUT_I2C);
     uint32_t press_data = 0;
     press_data = buffer[0] | (uint32_t) buffer[1] << 8 | (uint32_t) buffer[2] << 16;
     return (float)press_data/PRES_SENS;
 }
-float getTemperature() {
+float LPS22HH::getTemperature() {
     uint8_t buffer[2];
-    readBytes(CHIP_ADDRESS, TEMP_OUT_L, buffer, 2);
+    this->lpsReadBytes(TEMP_OUT_L, buffer, 2);
     int16_t temp_data = 0;
     temp_data = buffer[0] | (uint16_t) buffer[1] << 8;
     return (float)temp_data/TEMP_SENS;
 }
-uint8_t readWhoAmI() {
+uint8_t LPS22HH::readWhoAmI() {
     uint8_t buffer[1];
-    readBytes(CHIP_ADDRESS, WHO_AM_I, buffer, 1);
+    this->lpsReadBytes(WHO_AM_I, buffer, 1);
     return buffer[0];
 }
